9d5dc5ba70580fc155aafb6caa373453118539d0
xpetit <32063953+xpetit@users.noreply.github.com>
2020-11-26 12:19:35 +0100

    Revert "bypass buggy Go cache"
    This reverts commit 468fc04b36aabdfe1eac9f456c0a2988a2455152.

--- main.go
+++ main.go
@@ -228,7 +228,6 @@ func main() {
 	if b, err := exec.Command("git", "pull", "--ff-only").CombinedOutput(); err != nil {
 		os.Stderr.Write(b)
 	}
-	os.Remove("main.exe")
 	if b, err := exec.Command("go", "build", "-o", "main.exe", ".").CombinedOutput(); err != nil {
 		os.Stderr.Write(b)
 	}
